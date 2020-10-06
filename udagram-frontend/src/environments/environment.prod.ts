e5dbd49d34914653afff8c04443251757be24ccc
Rob Arnhart <rob@arnhart.com>
2020-10-05 20:56:29 -0500

    Updated apiHost and added required files for ionic

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://restapi.udagram.robarnhart.com/api/v0'
 };
 
 /*
